789b3319aa8766e762d720df8c283c575643ba68
pikopiri127 <hb.ucfbootcamp@gmail.com>
2018-11-13 10:54:51 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
