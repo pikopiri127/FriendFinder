eb8c04c763099586d015729b8acece34f53188a0
pikopiri127 <hb.ucfbootcamp@gmail.com>
2018-11-13 11:37:41 -0500

    update server.js

--- server.js
+++ server.js
@@ -6,4 +6,10 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
