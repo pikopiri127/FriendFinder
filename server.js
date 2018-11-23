fc09a1b7eb65bbc97cad4c64abede215ae91f703
pikopiri127 <hb.ucfbootcamp@gmail.com>
2018-11-22 21:39:19 -0500

    still...

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // ================================================================================
 
 require("./app/routing/apiRoutes")(app);
-require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
